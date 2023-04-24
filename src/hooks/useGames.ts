5ba8174a333db54458a91859fb88388f25bff61e
Justin GOff <claybball12@aol.com>
2023-04-23 21:16:15 -0400

    Fixed platform selector

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,7 @@ const useGames = (
 ) =>
   useData<Game>(
     "/games",
-    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder } },
+    { params: { genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder } },
     [gameQuery]
   );
 
